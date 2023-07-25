025a58dd3a4185cbc77448b86cff5f69ef631ec0
Rafal Krol <ameotoko1+github@gmail.com>
2023-07-25 08:58:11 +0200

    chore: deleted commented out attributes

--- .projenrc.ts
+++ .projenrc.ts
@@ -16,11 +16,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   license: 'MIT',
   keywords: ['awscdk', 'aws', 'cdk', 'fsx', 'ontap'],
   gitignore: ['.DS_Store', 'cdk.out'],
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 
 project.synth();
