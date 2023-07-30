1b5ab957d07de8c7b7c8050168b3a792d09938d7
Rafal Krol <ameotoko1+github@gmail.com>
2023-07-30 15:10:29 +0200

    chore: resuffle

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,12 +4,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Rafal Krol',
   authorAddress: 'ameotoko1+github@gmail.com',
   homepage: 'https://rafalkrol.xyz',
+  repositoryUrl: 'https://github.com/rafalkrol-xyz/cdk-fsx-ontap',
   cdkVersion: '2.85.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
   name: 'cdk-fsx-ontap',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/rafalkrol-xyz/cdk-fsx-ontap',
   publishToPypi: {
     distName: 'cdk-fsx-ontap',
     module: 'cdk_fsx_ontap',
